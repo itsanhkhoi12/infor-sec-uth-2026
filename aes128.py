6ea358fb4676743d98d8afd0bba5cbc855ac0be1
itsanhkhoi12 <anhkhoi5602@gmail.com>
2026-04-21 23:23:14 +0700

    feat: added decryption of AES128

--- aes128.py
+++ aes128.py
@@ -2,6 +2,25 @@ RCON = (
             0x00, 0x01, 0x02, 0x04, 0x08, 
             0x10, 0x20, 0x40, 0x80, 0x1B, 0x36)
 
+INV_SBOX = (
+        0x52, 0x09, 0x6a, 0xd5, 0x30, 0x36, 0xa5, 0x38, 0xbf, 0x40, 0xa3, 0x9e, 0x81, 0xf3, 0xd7, 0xfb,
+        0x7c, 0xe3, 0x39, 0x82, 0x9b, 0x2f, 0xff, 0x87, 0x34, 0x8e, 0x43, 0x44, 0xc4, 0xde, 0xe9, 0xcb,
+        0x54, 0x7b, 0x94, 0x32, 0xa6, 0xc2, 0x23, 0x3d, 0xee, 0x4c, 0x95, 0x0b, 0x42, 0xfa, 0xc3, 0x4e,
+        0x08, 0x2e, 0xa1, 0x66, 0x28, 0xd9, 0x24, 0xb2, 0x76, 0x5b, 0xa2, 0x49, 0x6d, 0x8b, 0xd1, 0x25,
+        0x72, 0xf8, 0xf6, 0x64, 0x86, 0x68, 0x98, 0x16, 0xd4, 0xa4, 0x5c, 0xcc, 0x5d, 0x65, 0xb6, 0x92,
+        0x6c, 0x70, 0x48, 0x50, 0xfd, 0xed, 0xb9, 0xda, 0x5e, 0x15, 0x46, 0x57, 0xa7, 0x8d, 0x9d, 0x84,
+        0x90, 0xd8, 0xab, 0x00, 0x8c, 0xbc, 0xd3, 0x0a, 0xf7, 0xe4, 0x58, 0x05, 0xb8, 0xb3, 0x45, 0x06,
+        0xd0, 0x2c, 0x1e, 0x8f, 0xca, 0x3f, 0x0f, 0x02, 0xc1, 0xaf, 0xbd, 0x03, 0x01, 0x13, 0x8a, 0x6b,
+        0x3a, 0x91, 0x11, 0x41, 0x4f, 0x67, 0xdc, 0xea, 0x97, 0xf2, 0xcf, 0xce, 0xf0, 0xb4, 0xe6, 0x73,
+        0x96, 0xac, 0x74, 0x22, 0xe7, 0xad, 0x35, 0x85, 0xe2, 0xf9, 0x37, 0xe8, 0x1c, 0x75, 0xdf, 0x6e,
+        0x47, 0xf1, 0x1a, 0x71, 0x1d, 0x29, 0xc5, 0x89, 0x6f, 0xb7, 0x62, 0x0e, 0xaa, 0x18, 0xbe, 0x1b,
+        0xfc, 0x56, 0x3e, 0x4b, 0xc6, 0xd2, 0x79, 0x20, 0x9a, 0xdb, 0xc0, 0xfe, 0x78, 0xcd, 0x5a, 0xf4,
+        0x1f, 0xdd, 0xa8, 0x33, 0x88, 0x07, 0xc7, 0x31, 0xb1, 0x12, 0x10, 0x59, 0x27, 0x80, 0xec, 0x5f,
+        0x60, 0x51, 0x7f, 0xa9, 0x19, 0xb5, 0x4a, 0x0d, 0x2d, 0xe5, 0x7a, 0x9f, 0x93, 0xc9, 0x9c, 0xef,
+        0xa0, 0xe0, 0x3b, 0x4d, 0xae, 0x2a, 0xf5, 0xb0, 0xc8, 0xeb, 0xbb, 0x3c, 0x83, 0x53, 0x99, 0x61,
+        0x17, 0x2b, 0x04, 0x7e, 0xba, 0x77, 0xd6, 0x26, 0xe1, 0x69, 0x14, 0x63, 0x55, 0x21, 0x0c, 0x7d
+)
+
 SBOX = (
     0x63, 0x7C, 0x77, 0x7B, 0xF2, 0x6B, 0x6F, 0xC5, 0x30, 0x01, 0x67, 0x2B, 0xFE, 0xD7, 0xAB, 0x76,
     0xCA, 0x82, 0xC9, 0x7D, 0xFA, 0x59, 0x47, 0xF0, 0xAD, 0xD4, 0xA2, 0xAF, 0x9C, 0xA4, 0x72, 0xC0,
@@ -21,20 +40,17 @@ SBOX = (
     0x8C, 0xA1, 0x89, 0x0D, 0xBF, 0xE6, 0x42, 0x68, 0x41, 0x99, 0x2D, 0x0F, 0xB0, 0x54, 0xBB, 0x16,
 )
 
-def gmul(a,coeff):
-    if coeff == 1:
-        return a
-    elif coeff == 2:
-        # GF(2^8) mult by 0x02: shift left 1, XOR 0x1b if MSB=1 (xtime)        
-        temp = ((a<<1) & 0xff)
+def gmul(val, coeff):
+    p = 0
+    for c in range(8):
+        if coeff & 1:
+            p ^= val
+        val <<= 1
+        if val & 0x100:
+            val ^= 0x11b
+        coeff >>= 1
+    return p
         
-        if a & 0x80:
-            temp ^= 0x1b
-        return temp
-    
-    else:
-        return a ^ gmul(a,2)
-
 def transpose_matrix(matrix: list[list[any]]):
     return [[matrix[j][i] for j in range(len(matrix))] for i in range(len(matrix[0]))]
     
@@ -45,34 +61,68 @@ def text_to_matrix(text: str)-> list[list[int]]:
         matrix[i%4].append(byte)
     return matrix     
 
+def hex_to_matrix(hex_text: str) -> list[list[int]]:
+    matrix = [[], [], [], []]
+    for i in range(16):
+        byte = int(hex_text[2 * i: 2 * i + 2], 16)
+        matrix[i % 4].append(byte)
+    return matrix
+
 def matrix_to_text(matrix: list[list[int]]) -> str:
     return ''.join(hex(matrix[i][j])[2:].zfill(2) for j in range(4) for i in range(4))
 
+def matrix_to_ascii(matrix: list[list[int]]) -> str:
+    return ''.join(chr(matrix[i][j]) for j in range(4) for i in range(4))
+
 class AES128:
     def __init__(self, secret_key: str):
-        if len(secret_key) != 16:
-            raise ValueError('Length of input key must be equal 128bit!')
+        self.n_k = len(secret_key)
+        if self.n_k not in (16,24,32):
+            raise ValueError('Length of input key must be in (128bit,192bit,256bit)!')
         self.change_keys(secret_key)
     
-    def __sub_bytes(self,state_matrix: list[list[str]]):
+    def __sub_bytes(self,state_matrix: list[list[int]]):
         for i in range(len(state_matrix)):
             for j in range(len(state_matrix)):
                 state_matrix[i][j] = SBOX[state_matrix[i][j]]
 
-    def __shift_rows(self, state_matrix: list[list[str]]):
+    def __sub_bytes_inv(self,state_matrix: list[list[int]]):
+        for i in range(len(state_matrix)):
+            for j in range(len(state_matrix)):
+                state_matrix[i][j] = INV_SBOX[state_matrix[i][j]]
+
+
+    def __shift_rows_inv(self, state_matrix: list[list[int]]):
+        for i in range(1,4):
+            state_matrix[i] = state_matrix[i][-i:] + state_matrix[i][:-i]
+
+    def __shift_rows(self, state_matrix: list[list[int]]):
         for i in range(1,4):
             state_matrix[i] = state_matrix[i][i:] + state_matrix[i][:i]
-
-    def __mix_single_columns(self, *col: list[int]):
-        a = gmul(col[0],2) ^ gmul(col[1],3) ^ col[2] ^ col[3]
+    
+    def __mix_single_column(self, *col: list[int]):
+        a = gmul(col[0],0x02) ^ gmul(col[1],0x03) ^ col[2] ^ col[3]
         b = col[0] ^ gmul(col[1],2) ^ gmul(col[2],3) ^ col[3]
-        c = col[0] ^ col[1] ^ gmul(col[2],2) ^ gmul(col[3],3)
-        d = gmul(col[0],3) ^ col[1] ^ col[2] ^ gmul(col[3],2)
+        c = col[0] ^ col[1] ^ gmul(col[2],0x02) ^ gmul(col[3],0x03)
+        d = gmul(col[0],0x03) ^ col[1] ^ col[2] ^ gmul(col[3],0x02)
         return a, b, c, d
 
-    def __mix_columns(self, state_matrix: list[list[str]]):
+    def __mix_columns(self, state_matrix: list[list[int]]):
         for i in range(4):
-            state_matrix[0][i], state_matrix[1][i], state_matrix[2][i], state_matrix[3][i] = self.__mix_single_columns(state_matrix[0][i], state_matrix[1][i], state_matrix[2][i], state_matrix[3][i])
+            state_matrix[0][i], state_matrix[1][i], state_matrix[2][i], state_matrix[3][i] = self.__mix_single_column(state_matrix[0][i], state_matrix[1][i], state_matrix[2][i], state_matrix[3][i])
+
+    def __mix_single_column_inv(self, *col: list[int]):
+       a = gmul(col[0],0x0e) ^ gmul(col[1],0x0b) ^ gmul(col[2],0x0d) ^ gmul(col[3],0x09)
+       b = gmul(col[0],0x09) ^ gmul(col[1],0x0e) ^ gmul(col[2],0x0b) ^ gmul(col[3],0x0d)
+       c = gmul(col[0],0x0d) ^ gmul(col[1],0x09) ^ gmul(col[2],0x0e) ^ gmul(col[3],0x0b)
+       d = gmul(col[0],0x0b) ^ gmul(col[1],0x0d) ^ gmul(col[2],0x09) ^ gmul(col[3],0x0e)
+
+       return a, b, c, d
+
+    def __mix_columns_inv(self, state_matrix: list[list[int]]):
+        for i in range(4):
+            state_matrix[0][i], state_matrix[1][i], state_matrix[2][i], state_matrix[3][i] = self.__mix_single_column_inv(state_matrix[0][i], state_matrix[1][i], state_matrix[2][i], state_matrix[3][i])
+
 
     def change_keys(self, secret_key):
         self.round_keys = transpose_matrix(text_to_matrix(secret_key))
@@ -80,11 +130,11 @@ class AES128:
             self.round_keys.append([])
 
             if i%4==0:
-                first_temp_byte = self.round_keys[i-4][0] ^ SBOX[self.round_keys[i-1][1]] ^ RCON[i//4]
+                first_temp_byte = self.round_keys[i-4][0] ^ SBOX[self.round_keys[i-1][1]]^ RCON[i//4]
                 self.round_keys[i].append(first_temp_byte)
 
                 for j in range(1,4):
-                    temp_byte = SBOX[self.round_keys[i-1][(j+1)%4]] ^ self.round_keys[i-4][j]
+                    temp_byte = SBOX[self.round_keys[i-1][(j+1)%4]]^ self.round_keys[i-4][j]
                     self.round_keys[i].append(temp_byte)
 
             else:
@@ -107,7 +157,34 @@ class AES128:
         self.__mix_columns(state_matrix)
         self.__add_round_key(state_matrix, round_key_matrix)
 
+    def __round_decrypt(self, state_matrix, round_key_matrix):
+        self.__add_round_key(state_matrix, round_key_matrix)
+        self.__mix_columns_inv(state_matrix)
+        self.__shift_rows_inv(state_matrix)
+        self.__sub_bytes_inv(state_matrix)
+
+    def decrypt(self, ciphertext: str):
+        self.cipher_state_matrix = hex_to_matrix(ciphertext)
+
+        self.__add_round_key(self.cipher_state_matrix, self.round_keys[40:])
+        self.__shift_rows_inv(self.cipher_state_matrix)
+        self.__sub_bytes_inv(self.cipher_state_matrix)
+
+        for i in range(9, 0, -1):
+            self.__round_decrypt(self.cipher_state_matrix, self.round_keys[4 * i : 4 * (i + 1)])
+
+        self.__add_round_key(self.cipher_state_matrix, self.round_keys[:4])
+
+        return matrix_to_ascii(self.cipher_state_matrix)
+
+
+
     def encrypt(self, plaintext: str):
+        self.n_b = len(plaintext)
+        
+        if self.n_b not in (16,24,32):
+            raise ValueError('Length of plaintext must be in (128bit,192bit,256bit)!')
+        
         self.state_matrix = text_to_matrix(plaintext)
         
         # Initialize State Matrix before Round 1
@@ -121,4 +198,4 @@ class AES128:
         self.__add_round_key(self.state_matrix, self.round_keys[40:])
         
         return matrix_to_text(self.state_matrix) 
-
+    
